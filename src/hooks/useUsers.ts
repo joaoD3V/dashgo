8b7e4275017c867a83db1317a6823c983f4b97ad
Joao Silva <joao.devweb@gmail.com>
2022-08-17 11:58:43 -0300

    Chapter IV - Lesson 02 finished :rocket:

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,36 @@
+import { useQuery } from 'react-query';
+import { UserData } from '../pages/users';
+import { api } from '../services/api';
+
+export async function getUsers(page: number) {
+  const { data, headers } = await api.get('users', {
+    params: {
+      page,
+    },
+  });
+
+  const totalCount = Number(headers['x-total-count']);
+
+  const users: UserData[] = data.users.map((user: UserData) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    };
+  });
+
+  return { users, totalCount };
+}
+
+export function useUsers(page: number) {
+  return useQuery(
+    ['users', page],
+    () => getUsers(page),
+    { staleTime: 1000 * 50 * 10 } // 10 minutos
+  );
+}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,18 +13,44 @@ import {
   Tr,
   Text,
   useBreakpointValue,
+  Spinner,
+  Link,
 } from '@chakra-ui/react';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { User } from '../../services/mirage';
+import { useUsers } from '../../hooks/useUsers';
+import { useState } from 'react';
+import { queryClient } from '../../services/queryClient';
+import { api } from '../../services/api';
+
+export type UserData = {
+  createdAt: string;
+} & Omit<User, 'created_at'>;
 
 export default function UserList() {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, error, isFetching } = useUsers(page);
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(
+      ['user', userId],
+      async () => {
+        const response = await api.get(`users/${userId}`);
+
+        return response.data;
+      },
+      { staleTime: 1000 * 60 * 10 } // 10 minutos
+    );
+  }
 
   return (
     <Box>
@@ -35,9 +61,12 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -47,145 +76,83 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && (
-                  <>
-                    <Th>Data de cadastro</Th>
-                    <Th w="8"></Th>
-                  </>
-                )}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">João Pedro</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      joao.devweb@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (
-                  <>
-                    <Td>04 de Abril, 2022</Td>
-                    <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                      >
-                        Editar
-                      </Button>
-                    </Td>
-                  </>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">João Pedro</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      joao.devweb@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (
-                  <>
-                    <Td>04 de Abril, 2022</Td>
-                    <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                      >
-                        Editar
-                      </Button>
-                    </Td>
-                  </>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">João Pedro</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      joao.devweb@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (
-                  <>
-                    <Td>04 de Abril, 2022</Td>
-                    <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                      >
-                        Editar
-                      </Button>
-                    </Td>
-                  </>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">João Pedro</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      joao.devweb@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (
-                  <>
-                    <Td>04 de Abril, 2022</Td>
-                    <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                      >
-                        Editar
-                      </Button>
-                    </Td>
-                  </>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && (
+                      <>
+                        <Th>Data de cadastro</Th>
+                        <Th w="8"></Th>
+                      </>
+                    )}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.users.map((user: UserData) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Link
+                            color="purple.400"
+                            onMouseEnter={() => handlePrefetchUser(user.id)}
+                          >
+                            <Text fontWeight="bold">{user.name}</Text>
+                          </Link>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && (
+                        <>
+                          <Td>{user.createdAt}</Td>
+                          <Td>
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="purple"
+                              leftIcon={
+                                <Icon as={RiPencilLine} fontSize="16" />
+                              }
+                            >
+                              Editar
+                            </Button>
+                          </Td>
+                        </>
+                      )}
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
